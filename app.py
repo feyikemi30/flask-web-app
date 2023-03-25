5aa5d77e2291ad50ff9c8ca8a9fc4edd3cf6d2a1
feyikemi30 <feyikemi30@yahoo.com>
2023-03-25 12:25:00 -0400

    new update

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
  return "I am almost a Devops Engineer!"
+
 if __name__ == "__main__":
  app.run(host='0.0.0.0')
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
