3ace4a245f626172da35eea475ae605e25e46da7
feyikemi30 <124748334+feyikemi30@users.noreply.github.com>
2023-03-25 13:34:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-app.debug = True
-
 @app.route("/")
 def index():
  return "I am almost a Devops Engineer!"
@@ -12,10 +9,3 @@ def index():
 if __name__ == '__main__':
  app.run(host='0.0.0.0')
 =======
-@app.route("/")
-def hello():
- return "I am almost a Devops Engineer!"
->>>>>>> 5aa5d77e2291ad50ff9c8ca8a9fc4edd3cf6d2a1
-
-if __name__ == "__main__":
- app.run(host='0.0.0.0')
