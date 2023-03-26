fe91ebbf19f4d397ef2a1c47547047ac9464ffad
feyikemi30 <124748334+feyikemi30@users.noreply.github.com>
2023-03-25 21:54:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
- return "I am almost a Devops Engineer!"
+def hello():
+   return "I am almost a Devops Engineer!"
 
 if __name__ == '__main__':
- app.run(host='0.0.0.0')
-=======
+
+port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, host='0.0.0.0', port=port)
+ 
