490058c0c35f5cec0a2946b4fa8ff1f1330ad4d1
feyikemi30 <feyikemi30@yahoo.com>
2023-03-26 18:43:01 -0400

    flask fix

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,14 +6,4 @@ def hello():
    return "I am almost a Devops Engineer!"
 
 if __name__ == '__main__':
-<<<<<<< HEAD
- 
-port = int(os.environ.get("PORT", 5000))
-   app.run(debug=True, host='0.0.0.0', port=port)
-
-=======
-
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
- 
->>>>>>> 8897e20924f3aa72cd98dc29452f4ba64b565b46
+   app.run(host='0.0.0.0')
